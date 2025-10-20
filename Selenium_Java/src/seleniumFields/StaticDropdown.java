a95450adc701fba2b64ef71268a421f1de2868c6
Kanniya Vikashini <kanniyavikashini@gmail.com>
2025-10-20 16:49:42 +0530

    static dropdown

---
+++ Selenium_Java/src/seleniumFields/StaticDropdown.java
@@ -0,0 +1,27 @@
+package seleniumFields;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class StaticDropdown {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		WebElement currency = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+		
+		Select dropdown = new Select(currency);
+		dropdown.selectByValue("USD");
+		dropdown.getFirstSelectedOption();
+		
+		driver.quit();
+		
+	}
+
+}
