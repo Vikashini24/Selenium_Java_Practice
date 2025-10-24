434221149f0183d68d872a67739c170006919654
Vikashini24 <kanniyavikashini@gmail.com>
2025-10-24 09:50:50 +0530

    implicit wait

---
+++ Selenium_Java/src/syncronization/ImplicitWait.java
@@ -0,0 +1,30 @@
+package syncronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		
+		driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
+		driver.findElement(By.className("cart-icon")).click();
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
+		
+		driver.findElement(By.className("promoBtn")).click();
+		String promoInfo = driver.findElement(By.className("promoInfo")).getText();
+		System.out.println(promoInfo);
+		
+	}
+
+}
