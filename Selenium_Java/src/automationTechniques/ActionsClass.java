e3031d510afb281836eb228f2b1bec4e643122a2
Kanniya Vikashini <kanniyavikashini@gmail.com>
2025-10-26 07:58:47 +0530

    action class

--- Selenium_Java/src/automationTechniques/ActionsClass.java
+++ Selenium_Java/src/automationTechniques/ActionsClass.java
@@ -15,15 +15,19 @@ public class ActionsClass {
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		
+		//driver.findElement(By.className("a-button-text")).click();
+		
 		Actions action = new Actions(driver);
-		//WebElement accounts = driver.findElement(By.id("nav-link-accountList"));
-		//action.moveToElement(accounts).build().perform();
 		
 		WebElement searchbox = driver.findElement(By.id("twotabsearchtextbox"));
 		action.moveToElement(searchbox).click().keyDown(Keys.SHIFT).sendKeys("books").doubleClick().build().perform();
+		
+		WebElement accounts = driver.findElement(By.id("nav-link-accountList"));
+		action.moveToElement(accounts).contextClick().build().perform();
+		
 	}
 
 }
-
+ 
 // build --> ready to execute
 // perform --> now it will execute
